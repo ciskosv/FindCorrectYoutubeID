6842bd30d2a7cec6cd59518cfc89557cf0aa231f
Stefan Rivera <cisko.sv@gmail.com>
2021-02-08 17:02:13 -0600

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,18 +1,19 @@
 import requests
-import time
-import random
+import itertools as it
 
-encontrado = 0
 count = 0
-while encontrado == 0:
+st ="XXXXX"   #Edit string here
 
-	s = 'AHO95BWZYWW'
-	u =''.join(random.random() > 0.5 and x.lower() or x for x in s)
-	r = requests.get(f'https://img.youtube.com/vi/{u}/mqdefault.jpg')
+comboList = list(map(''.join, it.product(*zip(st.upper(), st.lower()))))
+finalList = list(dict.fromkeys(comboList))
 
-	if r.status_code != 404:
-		print(f"FOUND: {u} {r.status_code}")
+for i in range(0,len(finalList)):
+
+	r = requests.get(f'https://img.youtube.com/vi/{finalList[i]}/mqdefault.jpg')
+
+	if r.status_code == 200:
+		print(f"FOUND: {finalList[i]} {r.status_code}")
 		break
 	else:
-		print(f"{count} - Bad request: {r.status_code} {u}")
+		print(f"{count} - Bad request: {r.status_code} {finalList[i]}")
 		count += 1
